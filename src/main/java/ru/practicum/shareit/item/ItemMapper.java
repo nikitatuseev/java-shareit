b51cd2bb213ba69882a9bba13c7109e5960230ee
nikitatuseev <tuseevn@mail.ru>
2023-06-14 15:03:29 +0300

    тз13

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -1,9 +1,9 @@
 package ru.practicum.shareit.item;
 
-
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
+
 @Component
 public class ItemMapper {
     public ItemDto toItemDto(Item item) {
